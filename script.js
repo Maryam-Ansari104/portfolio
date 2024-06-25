84451e80273d9bde91f5956554267344bbcdab8f
Maryam Ansari <112009910+Maryam-Ansari104@users.noreply.github.com>
2024-06-25 22:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let cvBtn = document.getElementById("cvBtn");
+let submit =document.getElementById("submit");
+let bar =document.getElementById("bar");
+
+
+cvBtn.addEventListener("click", function(){
+    alert("Downlode Successfully");
+})
+
+submit.addEventListener("click", function(){
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    if(email.value == "" && pass.value == ""){
+        alert("Fill Details")
+    }else{
+        alert("Logged IN")
+    }
+})
+
+let ul = document.querySelector("ul");
+bar.addEventListener("click", function(){
+    ul.classList.toggle("showData");
+
+    if(ul.className == "showData"){
+        bar.className="fa-solid fa-xmark"
+    }else{
+        bar.className="fa-solid fa-bars"
+    }
+})
